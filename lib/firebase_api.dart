be22b6861b9d42e2243a7fbc040b5bb5f4befce2
EsAm1973 <me354172@gmail.com>
2025-12-13 23:15:44 +0200

    Add firebase to configure messaging

---
+++ lib/firebase_api.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('Title: ${message.notification?.title}');
+  print('Body: ${message.notification?.body}');
+  print('Payload: ${message.data}');
+}
+
+class FirebaseApi {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications',
+    importance: Importance.defaultImportance,
+  );
+
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+    await _firebaseMessaging.subscribeToTopic('general');
+
+    // init local notifications
+    await initLocalNotifications(); // For foreground handling
+
+    // Setup Background Handler
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
+    // Handle Foreground Messages
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+
+      _localNotifications.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@mipmap/ic_launcher', // Using default icon
+          ),
+        ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
+
+    // Handle initial message (Terminated state)
+    final initialMessage = await _firebaseMessaging.getInitialMessage();
+    if (initialMessage != null) {
+      // You can navigate here if needed:
+      // Navigator.of(context).pushNamed(...)
+      print('App opened from terminated state: ${initialMessage.messageId}');
+    }
+
+    // Handle App Opened (Background state)
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      print('App opened from background state: ${message.messageId}');
+      // Navigator.of(context).pushNamed(...)
+    });
+  }
+
+  Future<void> initLocalNotifications() async {
+    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const ios = DarwinInitializationSettings(); // Use this for iOS
+    const settings = InitializationSettings(android: android, iOS: ios);
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (payload) {
+        // Handle local notification tap
+        final message = RemoteMessage.fromMap(jsonDecode(payload.payload!));
+        print('Local Notification Tapped: ${message.messageId}');
+      },
+    );
+
+    final platform = _localNotifications
+        .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin
+        >();
+    await platform?.createNotificationChannel(_androidChannel);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:samiocare_app/firebase_options.dart';
 
 import 'samiocare_app.dart';
+import 'firebase_api.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
   runApp(const SamiocareApp());
 }
